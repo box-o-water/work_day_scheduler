1cfb1841a70314cb9f20da9f22731b1e8ad20ed0
box-o-water <boxooowater@gmail.com>
2022-11-26 13:59:00 -0600

    feat(local-storage): working for all individual timeblocks

--- script.js
+++ script.js
@@ -14,24 +14,44 @@ var time14 = document.getElementById("14");
 var time15 = document.getElementById("15");
 var time16 = document.getElementById("16");
 
-var timesId = [time08.id, time09.id, time10.id, time11.id, time12.id, time13.id, time14.id, time15.id, time16.id]
-
 var times = [time08, time09, time10, time11, time12, time13, time14, time15, time16]
 
+var timesId = [time08.id, time09.id, time10.id, time11.id, time12.id, time13.id, time14.id, time15.id, time16.id]
+
+// The timeIndicator function indicates which timeblocks are past, present, or future
 function timeIndicator() {
-    console.log(timesId);
-    console.log(times);
+    // console.log(timesId);
+    // console.log(times);
     for (var i = 0; i < times.length; i++) {
         // add correct class, appending to current classes i/a
         if (timesId[i] < currentHour) {
-            times[i].className += times[i].className ? ' past' : 'past';
+            times[i].classList.add('past')
         } else if (timesId[i] > currentHour) {
-            times[i].className += times[i].className ? ' future' : 'future';
+            times[i].classList.add('future')
         } else {
-            times[i].className += times[i].className ? ' present' : 'present';
+            times[i].classList.add('present')
         }
     }
 };
+
+// This function saves and retrieves all events with local storage, once the document is loaded
+$(document).ready(function() {
+    $('.saveBtn').on('click', function() {
+        // get the id of the parent
+        var thisTime = $(this).parent().attr('id');
+        // get the value of the sibling with a description id
+        var thisEvent = $(this).siblings('.description').val();
+
+        // send the time and event to local storage
+        localStorage.setItem(thisTime, thisEvent);
+    });
+
+    for (var i = 0; i < timesId.length; i++) {
+        // retrieve the time and event from local storage, populating the event text 
+        $(`#${timesId[i]} .description`).val(localStorage.getItem(`${timesId[i]}`));
+    }
+
+});
 
 function init() {
     timeIndicator()
